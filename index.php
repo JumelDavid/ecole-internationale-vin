75bfad1e2e820edb4d734bf52efe84b49b81ecd9
JumelDavid <david.jumel@ynov.com>
2016-04-04 22:12:50 +0200

    Added files via upload

--- index.php
+++ index.php
@@ -1,117 +1,66 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<?php require 'includes/head.php'; ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>EIV</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+	 <link href="css/styles.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
 
-<body>
+<body style="padding-top:0;">
+	<section id="index" class="emerald" style="height: 90vh !important; text-align: center; margin: auto;">
+		<div class="container" style="text-align: center; margin: auto;">
+			<div class="row" style="text-align: center; margin: auto; height: 30vh;">
+				<div class="col-lg-12" style="text-align: center; margin: auto;">
+					<h1>SITUATION PROFESSIONNELLE: Ecole Internationale du Vin</h1>
+				</div>
+			</div>
+			<div class="row" style="text-align: center; margin: auto;">
+				<div class="col-lg-12" style="text-align: center; margin: auto;">
+					<div class="col-lg-4">
+						<button class="btn btn-default"><a href="" target="blank">DOSSIER DE PRESENTATION</a></button>
+					</div>
+						<div class="col-lg-4">
+							<button class="btn btn-default"><a href="" target="blank">ACCES AU CODE SOURCE</a></button>
+						</div>
+						<div class="col-lg-4">
+							<button class="btn btn-default"><a href="accueil.php" target="blank">ACCES AU SITE INTERNET</a></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-<?php require 'includes/navbar.php'; ?>
+<footer id="footer" class="midnight-blue" style="padding: 12px 0 3px 0">
+	 <div class="container">
+		  <div class="row">
+				<div class="col-sm-6">
+					 <p style="margin-top: 10px;"> 2015-2016 Ecole Internationale du Vin. All Rights Reserved.</p>
+				</div>
+		  </div>
+	 </div>
+</footer><!--/#footer-->
 
-    <section id="main-slider" class="no-margin">
-        <div class="carousel slide wet-asphalt">
-            <ol class="carousel-indicators">
-                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
-                <li data-target="#main-slider" data-slide-to="1"></li>
-                <li data-target="#main-slider" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="item active" style="background-image: url(images/slider/slide1.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="carousel-content centered">
-                                    <h2 class="animation animated-item-1">Ecole Internationale du Vin</h2>
-                                    <h4 class="animation animated-item-2">Bienvenue sur le site de l'EIV, découvrez notre formation</h4>
-												<br>
-												<a class="btn btn-md animation animated-item-3" href="ecole.php">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!--/.item-->
-                <div class="item item-2" style="background-image: url(images/slider/slide2.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="carousel-content centered">
-                                    <h2 class="boxed animation animated-item-1">Ecole Internationale du Vin</h2>
-                                    <h4 class="boxed animation animated-item-2">Bienvenue sur le site de l'EIV, découvrez notre formation</h4>
-                                    <br>
-                                    <a class="btn btn-md animation animated-item-3" href="ecole.php">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!--/.item-->
-                <div class="item item-3" style="background-image: url(images/slider/slide3.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="carousel-content centered">
-                                    <h2 class="boxed animation animated-item-1">Ecole Internationale du Vin</h2>
-                                    <h4 class="boxed animation animated-item-2">Bienvenue sur le site de l'EIV, découvrez notre formation</h4>
-                                    <a class="btn btn-md animation animated-item-3" href="ecole.php">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!--/.item-->
-            </div><!--/.carousel-inner-->
-        </div><!--/.carousel-->
-        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
-            <i class="icon-angle-left"></i>
-        </a>
-        <a class="next hidden-xs" href="#main-slider" data-slide="next">
-            <i class="icon-angle-right"></i>
-        </a>
-    </section><!--/#main-slider-->
-
-    <section id="services" class="emerald">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="media">
-                        <div class="pull-left">
-                            <i class="icon-glass icon-md"></i>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Etudier dans un cadre idéal</h3>
-                            <p>Première consommatrice et exportatrice de vin dans le monde, la France s'est imposée comme terre d'accueil d'une École dédiée
-										 à la formation d'oenologues confirmé(e)s. Située sur les bords de la Garonne, l'Ecole Internationale du Vin de Bordeaux (en Gironde)
-										 dispose de locaux à la pointe de la technologie et d'un environnement de travail comme nulle part ailleurs.</p>
-                        </div>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-lg-4">
-                    <div class="media">
-                        <div class="pull-left">
-                            <i class="icon-thumbs-up icon-md"></i>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Un secteur en constant développement</h3>
-                            <p>La filière viticole, dont les exigences professionnelles et internationales se sont fortement accrues, recherche des
-										 jeunes passionnés et avides d'apprendre. Le domaine de l'Oenologie étant en plein expension, nous misons sur l'avenir de la profession
-										 en proposant un cursus accessible dès l'obtention du Baccalauréat!</p>
-                        </div>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-lg-4">
-                    <div class="media">
-                        <div class="pull-left">
-                            <i class="icon-user icon-md"></i>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Un cursus evolutif</h3>
-                            <p>Le cursus de cette écolé est très simple. Sur 5 ans d'études, vos 2 premières années sont exclusivement consacrées à la découverte de cette
-										 filière viticole. La formation se déroulant en continue jusqu'au passage d'un examen validant un Bac+2 et vous ouvre la voie vers les
-										 3 années supérieures. Au cours de ces 3 années, vous aurez la possibilité de réaliser vos études en alternance. De nombreuses entreprises
-										 et grands chateaux, partenaires de notre école, vous accueilleront une semaine sur deux ainsi que pendant toutes les périodes de
-										 fermeture de l'école.</p>
-                        </div>
-                    </div>
-                </div><!--/.col-md-4-->
-            </div>
-        </div>
-    </section><!--/#services-->
-
-<?php require 'includes/footer.php'; ?>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
